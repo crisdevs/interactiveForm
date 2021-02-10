d8d543defa565dc26b76052cf1903e7325e21f2e
crisdevs <crisdevs@github.com>
2021-02-09 20:14:29 -0500

    Activities checkboxes
    When a user clicks any of the activities checkboxes, now shows the corresponding total

--- js/script.js
+++ js/script.js
@@ -3,39 +3,61 @@ const otherJobText = document.querySelector("#other-job-role");
 const tShirtColor = document.querySelector("#color");
 const tShirtDesign = document.querySelector("#design");
 const tShirtColorOpt = tShirtColor.querySelectorAll("option");
+const fieldActivities = document.querySelector("#activities");
+const activitiesTotalHTML = document.querySelector("#activities-cost");
+let activitiesTotal = 0;
+
 //Function for when the page loads
 const startUp = () => {
-    //Name input
+  //Name input
   const nameInput = document.querySelector("#name");
   //Puts focus on name input
   nameInput.focus();
-  //Removes the display of the other text field 
+  //Removes the display of the other text field
   otherJobText.style.display = "none";
   //disables the drop down for the tshirt color
   tShirtColor.disabled = true;
 };
 //Event listener for when the job drop down changes value
 jobDropDown.addEventListener("change", (e) => {
-    //if the other job drop down selection is selected
+  //if the other job drop down selection is selected
   if (e.target.value === "other") {
-      //The other job text field appears on the web page
+    //The other job text field appears on the web page
     otherJobText.style.display = "block";
   } else {
-      //else if another selection is selected then remove the other job text field in case it is visible on the web page
+    //else if another selection is selected then remove the other job text field in case it is visible on the web page
     otherJobText.style.display = "none";
   }
 });
 //Event listener in case a value is changed in the tshirt design drop down.
-tShirtDesign.addEventListener("change", (e) =>{
+tShirtDesign.addEventListener("change", (e) => {
   tShirtColor.disabled = false;
   tShirtColor.innerHTML = "";
-  for(let i =0; i < tShirtColorOpt.length; i++){
-    if(e.target.value === "js puns" && tShirtColorOpt[i].getAttribute("data-theme") === "js puns"){
+  for (let i = 0; i < tShirtColorOpt.length; i++) {
+    if (
+      e.target.value === "js puns" &&
+      tShirtColorOpt[i].getAttribute("data-theme") === "js puns"
+    ) {
+      tShirtColor.appendChild(tShirtColorOpt[i]);
+    } else if (
+      e.target.value === "heart js" &&
+      tShirtColorOpt[i].getAttribute("data-theme") === "heart js"
+    ) {
       tShirtColor.appendChild(tShirtColorOpt[i]);
     }
-    else if(e.target.value === "heart js" && tShirtColorOpt[i].getAttribute("data-theme") === "heart js"){
-      tShirtColor.appendChild(tShirtColorOpt[i]);
+  }
+});
+//Event Listener for the activities checkboxes
+fieldActivities.addEventListener("change", (e) => {
+  if (e.target.type === "checkbox") {
+    const activCost = e.target.getAttribute("data-cost");
+
+    if (e.target.checked === true) {
+      activitiesTotal += parseInt(activCost);
+    } else {
+      activitiesTotal -= parseInt(activCost);
     }
+    activitiesTotalHTML.textContent = `Total: $${activitiesTotal}`;
   }
 });
 
