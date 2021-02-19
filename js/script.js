20c44e19564e760c616ca77cc79711f578b6e16d
crisdevs <crisdevs@github.com>
2021-02-18 21:28:58 -0500

    Added comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Query Selectors
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const jobDropDown = document.querySelector("#title");
@@ -18,24 +19,32 @@ const creditCardNum = document.querySelector("#cc-num");
 const zipCodeNum = document.querySelector("#zip");
 const cvvNum = document.querySelector("#cvv");
 const form = fieldActivities.parentNode;
-
+//Total variable for activities
 let activitiesTotal = 0;
 
-//Function for when the page loads
+/**
+ * Puts focus on the name input, hides elements, disables t-shirt color selection element, and sets the value of the payment option to credit card.
+ */
 const startUp = () => {
   //Puts focus on name input
   nameInput.focus();
-  //Removes the display of the other text field
-  hideOrShow(otherJobText,"hide")
 
+  hideOrShow(otherJobText,"hide")
   hideOrShow(payPal,"hide");
   hideOrShow(bitCoin, "hide");
+
   //disables the drop down for the tshirt color
   tShirtColor.disabled = true;
 
   selectPayment.value = "credit-card";
 };
-
+/**
+ * Hides or Shows the given elements based on the string given.
+ *
+ * @param {HTML} element - The element to be hidden or shown.
+ * @param {string} command - A string to represent hide or show depending on the purpose.
+ */
+//Function to show or hide the given eleme
 const hideOrShow = (element, command) =>{
   if(command === "hide"){
     element.style.display = "none";
@@ -47,61 +56,75 @@ const hideOrShow = (element, command) =>{
     console.log("Invalid command");
   }
 }
+
+/**
+ *Applies styling and will prevent the form from submiting depending on the outcome of the input value and the regex test.
+ *
+ * @param {regex} regexTest - The regex test to test the inputs.
+ * @param {HTML} input - The input whose value will be tested against the passed regex.
+ * @param {object} eventObj - The event target to be used to prevent the submition of the page
+ */
 const checkInput = (regexTest, input, eventObj) => {
   if (regexTest.test(input.value)) {
     input.parentNode.className += " valid";
     input.parentNode.classList.remove("not-valid");
-    input.nextElementSibling.style.display = "none";
+    hideOrShow(input.nextElementSibling, "hide");
   } else {
     eventObj.preventDefault();
     input.parentNode.className += " not-valid";
-    input.nextElementSibling.style.display = "block";
+    hideOrShow(input.nextElementSibling, "show");
   }
 };
 
 //Event listener for when the job drop down changes value
 jobDropDown.addEventListener("change", (e) => {
-  //if the other job drop down selection is selected
   if (e.target.value === "other") {
-    //The other job text field appears on the web page
-    otherJobText.style.display = "block";
+    hideOrShow(otherJobText, "show");
   } else {
-    //else if another selection is selected then remove the other job text field in case it is visible on the web page
     hideOrShow(otherJobText, "hide");
   }
 });
 //Event listener in case a value is changed in the tshirt design drop down.
 tShirtDesign.addEventListener("change", (e) => {
   tShirtColor.disabled = false;
+  //Every time the t shirt design drop down changes will erase the options
   tShirtColor.innerHTML = "";
+  //For loop to loop through each of pulled t shirt color options stored in tShirtColorOpt(Array of HTML Options).
   for (let i = 0; i < tShirtColorOpt.length; i++) {
+    //If the selected t shirt design value matches the t shirt color option 'data-theme' attribute.
     if (e.target.value === tShirtColorOpt[i].getAttribute("data-theme")) {
       tShirtColor.appendChild(tShirtColorOpt[i]);
     }
   }
 });
-//Event Listener for the activities checkboxes
+//Event Listener that listens to any change in the fieldset element.
 fieldActivities.addEventListener("change", (e) => {
   if (e.target.type === "checkbox") {
     const activCost = e.target.getAttribute("data-cost");
-
+    //If the selected activity is being checked
     if (e.target.checked === true) {
+      //Converts the activity cost pulled from the 'data-cost' attribute from a string to a integer and adds to the total.
       activitiesTotal += parseInt(activCost);
-    } else {
+    }
+    //If it is not checked when clicked 
+    else {
+      //Converts the activity cost pulled from the 'data-cost' attribute from a string to a integer and subtracts to the total.
       activitiesTotal -= parseInt(activCost);
     }
+    //Adds the total to the HTML to be shown.
     activitiesTotalHTML.textContent = `Total: $${activitiesTotal}`;
   }
 });
-
+//Event Listener that listens to any change in the drop down select element.
 selectPayment.addEventListener("change", (e) => {
-  const typeOfPayment = document.getElementById(e.target.value);
+  //Grabs the 
+  // const typeOfPayment = document.getElementById(e.target.value);
 
   hideOrShow(payPal,"hide");
   hideOrShow(bitCoin, "hide");
   hideOrShow(creditCard,"hide");
 
-  hideOrShow(typeOfPayment, "show");
+  hideOrShow(e.target.value, "show");
 });
 
 form.addEventListener("submit", (e) => {
